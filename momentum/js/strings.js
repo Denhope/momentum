c5dec39975cb98cf564ec7569e3c1daf591066da
Denis_Koval <den_hope@mail.ru>
2021-10-24 22:27:20 +0300

    feat: add strings

---
+++ momentum/js/strings.js
@@ -0,0 +1,21 @@
+export const STRINGS = {
+    greetings: {
+        'ru': {
+            'morning': 'Доброе утро',
+            'afternoon': 'Добрый день',
+            'evening': 'Добрый вечер',
+            'night': 'Доброй ночи'
+        },
+        'en': {
+            'morning': 'Good morning',
+            'afternoon': 'Good afternoon',
+            'evening': 'Good evening',
+            'night': 'Good night'
+        }
+    },
+    greetingPlaceholder: {
+        'ru': '[Введите имя]',
+        'en': '[Enter name]'
+    },
+
+}
