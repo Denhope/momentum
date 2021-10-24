91268c127f29ce96821cf299f79e4ec3f36ea395
Denis_Koval <den_hope@mail.ru>
2021-10-24 22:40:44 +0300

    fix: fix clock.js

--- momentum/js/clock.js
+++ momentum/js/clock.js
@@ -12,9 +12,9 @@ function showDate(){
     const date = new Date();
     const dateString = document.querySelector('.date');
     const lang = localStorage.getItem('lang');
-    
+
     const options = {weekday: 'long', day: 'numeric', month: 'long',};
-    const currentDate = date.toLocaleDateString('en-En', options);       
+    const currentDate = date.toLocaleDateString('en-En',  options);       
     
     dateString.textContent = currentDate
 }
