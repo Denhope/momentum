0704e9e1ec68167c127c5dbcc435672cf98c53f2
Denis_Koval <den_hope@mail.ru>
2021-10-25 17:16:34 +0300

    feat: add humidity and wind speed

--- momentum/js/weather.js
+++ momentum/js/weather.js
@@ -1,21 +1,24 @@
 const weatherIcon = document.querySelector('.weather-icon');
 const temperature = document.querySelector('.temperature');
 const weatherDescription = document.querySelector('.weather-description');
-
+const wind = document.querySelector('.wind');
+const humidity = document.querySelector('.humidity');
 
 
 
 async function getWeather() {  
    
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=Минск&lang=ru&appid=bf5425e3d31eabbedb3c44fafb1cad2e&units=metric`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=Минск&lang=en&appid=bf5425e3d31eabbedb3c44fafb1cad2e&units=metric`;
     const res = await fetch(url);
     const data = await res.json(); 
-    console.log(data.weather[0].id, data.weather[0].description, data.main.temp);
+    // console.log(data.weather[0].id, data.weather[0].description, data.main.temp);
 
     weatherIcon.classList.add(`owf-${data.weather[0].id}`);
     temperature.textContent = `${data.main.temp}°C`;
     weatherDescription.textContent = data.weather[0].description;
+    wind.textContent = `Wind speed: ${data.wind.speed} m/s`;
+    humidity.textContent = `Humidity: ${data.main.humidity} %`;
     const cityField = document.querySelector('.city')
     cityField.value = localStorage.getItem('.city') || 'Минск'
     
