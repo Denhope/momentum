528901a70a62b82dfdef947fc92e2142062bd131
Denis_Koval <den_hope@mail.ru>
2021-10-25 14:43:45 +0300

    refactor: add weather file

---
+++ momentum/js/weather.js
@@ -0,0 +1,10 @@
+
+
+
+async function getWeather() {  
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=Минск&lang=ru&appid=08f2a575dda978b9c539199e54df03b0&units=metric`;
+    const res = await fetch(url);
+    const data = await res.json(); 
+    console.log(data.weather[0].id, data.weather[0].description, data.main.temp);
+  }
+  getWeather()
