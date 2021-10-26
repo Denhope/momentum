2a39485e713c138e60c4f454660445931de748fa
Denis_Koval <den_hope@mail.ru>
2021-10-26 03:01:05 +0300

    feat: update weather file

--- momentum/js/weather.js
+++ momentum/js/weather.js
@@ -3,30 +3,88 @@ const temperature = document.querySelector('.temperature');
 const weatherDescription = document.querySelector('.weather-description');
 const wind = document.querySelector('.wind');
 const humidity = document.querySelector('.humidity');
+const error = document.querySelector('.weather-error');
+const city = document.querySelector('.city');
 
 
 
-async function getWeather() {     
+async function getWeather() {
+    // try{
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=Минск&lang=en&appid=bf5425e3d31eabbedb3c44fafb1cad2e&units=metric`;
+        // const url =  `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=${lang}&appid=bf5425e3d31eabbedb3c44fafb1cad2e&units=metric`;
+        const res = await fetch(url);
+        const data = await res.json(); 
+      
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=Минск&lang=en&appid=bf5425e3d31eabbedb3c44fafb1cad2e&units=metric`;
-    const res = await fetch(url);
-    const data = await res.json(); 
-    // console.log(data.weather[0].id, data.weather[0].description, data.main.temp);
+        error.textContent = '';
+        weatherIcon.className = 'weather-icon owf';
+        weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+        temperature.textContent = `${data.main.temp}°C`;
+        weatherDescription.textContent = data.weather[0].description;
+        wind.textContent = `Wind speed: ${data.wind.speed} m/s`;
+        humidity.textContent = `Humidity: ${data.main.humidity} %`;
+        
+        city.value = localStorage.getItem('.city') || 'Minsk'
 
-    weatherIcon.classList.add(`owf-${data.weather[0].id}`);
-    temperature.textContent = `${data.main.temp}°C`;
-    weatherDescription.textContent = data.weather[0].description;
-    wind.textContent = `Wind speed: ${data.wind.speed} m/s`;
-    humidity.textContent = `Humidity: ${data.main.humidity} %`;
-    const cityField = document.querySelector('.city')
-    cityField.value = localStorage.getItem('.city') || 'Minsk'
+
+    // }   
+
+    // catch(err)  {
+    //     if (city.value.length == 0) {
+    //         error.textContent = ``;
+    //     } 
+    //     else {
+    //         error.textContent = `Error! City '${city.value}' not found!`;
+    //     }
+        
+    //     weatherIcon.className = 'weather-icon owf';
+    //     temperature.textContent = '';
+    //     weatherDescription.textContent = '';
+    //     wind.textContent = '';
+    //     humidity.textContent = '';
+    // }  
+  
+        
+}
+
+// function setCity(event) {
+//     if (event.code === 'Enter' && city.value.length == 0) {
+//         error.textContent = `Error! Enter some city!`;
+//     }
+// }
+
+// city.addEventListener('keypress', setCity);
     
-    
+
+city.addEventListener('change', getWeather);
+
+
+function setLocalStorage() {
+    localStorage.setItem('city', city.value);
+}
+window.addEventListener('beforeunload', setLocalStorage);
+
+
+
+function getLocalStorage() {
+    if(localStorage.getItem('city')) {
+      city.value = localStorage.getItem('city');
+      getWeather();
+    }
   }
-//   getWeather();
+window.addEventListener('load', getLocalStorage);
+
+
+
+
 
   export function init(){   
 
     getWeather();
+    // setCity();
+    setLocalStorage();
+    getLocalStorage();
+
+
 }
 
