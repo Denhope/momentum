8fa74ba327ebec8651d1842f505ac61672eddb1c
Denis_Koval <den_hope@mail.ru>
2021-10-26 10:40:17 +0300

    feat: update slider

--- momentum/js/slider.js
+++ momentum/js/slider.js
@@ -1,51 +1,66 @@
 import * as Greeting from "./greeting.js";
 
-function setListeners() {
-    const nextButton = document.querySelector('.slide-next');
-    const prevButton = document.querySelector('.slide-prev');
-   
-
-    nextButton.addEventListener('click', getSlideNext());
-    prevButton.addEventListener('click', getSlidePrew());
-};
 
 function getRandomNum() {
     let num = Math.floor(20 * Math.random()) + 1;
     return num;
 }
 
-console.log(getRandomNum())
-
 function setImage(){
     const timesOfDay = Greeting.getTimesOfDay();
     const bgNum = getRandomNum();
     const body = document.querySelector('.body');
     let imageUrl = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timesOfDay}/${bgNum.toString().padStart(2,'0')}.jpg`;
-    document.body.style.backgroundImage = `url(${imageUrl})`;
-    
+    document.body.style.backgroundImage = `url(${imageUrl})`;    
 
 }
 
+const slidePrev = document.querySelector('.slide-prev');
+const slideNext = document.querySelector('.slide-next');
+
+let randomNum = getRandomNum();
 
 
+function getSlideNext() {
+    randomNum = randomNum + 1 <= 20 ? randomNum - 1 : 1;
+    setImage()
+}
+
+function getSlidePrew() {
+    randomNum = randomNum - 1 > 0 ? randomNum - 1 : 20;
+    setImage()
+}
+// function getSlideNext() {
+//     if (randomNum < 20) {
+//       randomNum = randomNum + 1;
+//     } else {
+//       randomNum = 1;
+//     }
+//     setImage();  
+// }
 
 // function getSlidePrew() {
-//     this.current = this.current - 1 > 0 ? this.current - 1 : 20;
-//     this.updateImage()
+//     if (randomNum > 1) {
+//   randomNum = randomNum - 1;
+//     } else {
+//       randomNum = 20;
+//     }
+//     console.log(randomNum);
+//     setImage();
 // }
-
-// function getSlideNext() {
-//     this.current = this.current + 1 <= 20 ? this.current + 1 : 1
-//     this.updateImage()
-// }
+function setListeners() {
+    slideNext.addEventListener('click', () =>{
+        getSlideNext();
+      });
+      slidePrev.addEventListener('click', () =>{
+        getSlidePrew();
+      });
+}
 
 export function init(){ 
    
-    // getSlidePrew()
-    // getSlideNext()
-    // updateImage()
+    getSlidePrew()
+    getSlideNext()
     setImage()
-    // setListeners()
+    setListeners()
 }
-
-
