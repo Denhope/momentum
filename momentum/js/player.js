1875f8e1484aa012f1b8ece8aa383b77db956585
Denis_Koval <den_hope@mail.ru>
2021-10-27 16:12:08 +0300

    feat: reccomit player

--- momentum/js/player.js
+++ momentum/js/player.js
@@ -1,39 +1,39 @@
 
-// import playList from "./playList.js";
+import playList from "./playList.js";
 
-// export function init() {
+export function init() {
     
 
     
-//     const playListContainer = document.querySelector('.play-list');
+    const playListContainer = document.querySelector('.play-list');
 
 
 
-//     playList.forEach(el => {
-//         const li = document.createElement('li');// здесь ваш код
-//         playListContainer.append(li);
-//         li.classList.add('play-item');
-//         li.textContent = el.title;   
-//     })
+    playList.forEach(el => {
+        const li = document.createElement('li');// здесь ваш код
+        playListContainer.append(li);
+        li.classList.add('play-item');
+        li.textContent = el.title;   
+    })
 
 
 
-//     const playBtn = document.querySelector('.play');
-//     const prevBtn= document.querySelector('.play-prev');
-//     const nextBtn = document.querySelector('play-next');
+    const playBtn = document.querySelector('.play');
+    const prevBtn= document.querySelector('.play-prev');
+    const nextBtn = document.querySelector('play-next');
 
-//     const audio = document.querySelector('audio');
+    const audio = document.querySelector('audio');
 
     
-//     function playAudio() {
-//         audio.currentTime = 0;
-//         audio.play();
-//     }
-//     function pauseAudio() {
-//         audio.pause();
-//     }
+    function playAudio() {
+        audio.currentTime = 0;
+        audio.play();
+    }
+    function pauseAudio() {
+        audio.pause();
+    }
 
-// }
+}
 
 
 
