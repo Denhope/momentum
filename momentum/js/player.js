06cdfcc3af6f3b0be929cd98023eb96926b848d1
Denis_Koval <den_hope@mail.ru>
2021-10-27 15:08:54 +0300

    feat: add and update player file

---
+++ momentum/js/player.js
@@ -0,0 +1,15 @@
+
+import playList from "./playList.js";
+
+export function init() {   
+    
+    const playListContainer = document.querySelector('.play-list');
+    playList.forEach(el => {
+        const li = document.createElement('li');// здесь ваш код
+        playListContainer.append(li);
+        li.classList.add('play-item');
+        li.textContent = el.title;   
+    })
+}
+
+
